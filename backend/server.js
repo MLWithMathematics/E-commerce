ca7f736a0811964f11e76e0c8612f2f42c42d11d
MLWithMathematics <shubhankar.coder@gmail.com>
2026-04-01 22:01:15 +0530

    1

--- backend/server.js
+++ backend/server.js
@@ -53,9 +53,10 @@ app.use((err, req, res, next) => {
 });
 
 // Start
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+console.log("ENV PORT:", process.env.PORT);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
-  console.log("ENV PORT:", process.env.PORT);
 });
